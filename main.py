613b455bf3303380db02f59d208134ddbd0efbc9
armr0707hotmail <armr0707@hotmail.com>
2024-09-03 20:06:58 -0300

    atualização do main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Antonio Rogerio Machado Ramos ')
+    print_hi('Antonio Rogerio Machado Ramos')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
